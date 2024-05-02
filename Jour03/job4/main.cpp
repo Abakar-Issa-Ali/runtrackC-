88c83053e4b194694379583352e7014047d997d8
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-05-02 11:17:54 +0200

    job4 added

---
+++ Jour03/job4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string chaine = "vive la plateforme !";
+    int taille = chaine.length();
+          
+    cout << "La taille est " << taille << endl;
+
+    return 0;
+}
+
